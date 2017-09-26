7382fe5560e8a50f2afafd97d3d5b6b1bf0e256c
o Rafa <paracafa@gmail.com>
2017-09-26 17:29:01 -0300

    Criado activity para salvar new studant

---
+++ app/src/main/java/com/example/orafa/androidexercicioactivitiesintentsfacul/view/NewStudantActivity.java
@@ -0,0 +1,15 @@
+package com.example.orafa.androidexercicioactivitiesintentsfacul.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.orafa.androidexercicioactivitiesintentsfacul.R;
+
+public class NewStudantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_studant);
+    }
+}
