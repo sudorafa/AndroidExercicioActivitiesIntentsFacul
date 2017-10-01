0a26d0ef165f87df1fad53beec74b2459479a10d
o Rafa <paracafa@gmail.com>
2017-10-01 17:51:59 -0300

    Colocado açoes nas escolhas do Context Item Selected

--- app/src/main/java/com/example/orafa/androidexercicioactivitiesintentsfacul/view/MainActivity.java
+++ app/src/main/java/com/example/orafa/androidexercicioactivitiesintentsfacul/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.orafa.androidexercicioactivitiesintentsfacul.view;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -53,9 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreateContextMenu(menu, v, menuInfo);
         menu.add(Menu.NONE, 1, Menu.NONE, R.string.callToStudent);
         menu.add(Menu.NONE, 2, Menu.NONE, R.string.smsToStudent);
-        menu.add(Menu.NONE, 2, Menu.NONE, R.string.sendEmail);
-        menu.add(Menu.NONE, 3, Menu.NONE, R.string.mapLocation);
-        menu.add(Menu.NONE, 4, Menu.NONE, R.string.siteStudent);
+        menu.add(Menu.NONE, 3, Menu.NONE, R.string.sendEmail);
+        menu.add(Menu.NONE, 4, Menu.NONE, R.string.mapLocation);
+        menu.add(Menu.NONE, 5, Menu.NONE, R.string.siteStudent);
     }
 
     //Para poder dar uma ação para um context menu: Basta sobrescrevemos o método onContextItemSelected
@@ -63,7 +64,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case 1:
-
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + student.getPhone()));
+                startActivity(intentCall);
+                break;
+            case 2:
+                Intent intentSendSms = new Intent(Intent.ACTION_VIEW);
+                intentSendSms.putExtra("address", student.getPhone());
+                startActivity(intentSendSms);
+                break;
+            case 3:
+                Intent intentSendEmail = new Intent(Intent.ACTION_VIEW);
+                intentSendEmail.putExtra("address", student.getEmail());
+                startActivity(intentSendEmail);
+                break;
+            case 4:
+                String uriMap = "http://maps.google.com/maps";
+                Intent intentMap = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uriMap));
+                intentMap.setComponent(new ComponentName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity"));
+                startActivity(intentMap);
+                //Por exemplo, para exibir um mapa de São Francisco, você pode usar este código:
+//                Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+//                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//                mapIntent.setPackage("com.google.android.apps.maps");
+//                startActivity(mapIntent);
+                break;
+            case 5:
+                Intent intentSite = new Intent(Intent.ACTION_VIEW, Uri.parse(student.getSite()));
+                startActivity(intentSite);
+                break;
         }
         return super.onContextItemSelected(item);
     }
